5813654561d15c8bd224bb3d6474dc7204c0a6e4
Sara Strong <sara.n.strong@gmail.com>
2020-11-22 23:13:45 -0500

    Added advanced search

--- trefle_requests.py
+++ trefle_requests.py
@@ -1,4 +1,4 @@
-"""Functions for making and handling requests to Trefle API."""
+"""Functions for making requests to Trefle API."""
 
 import requests
 from secrets import trefle_token
@@ -6,11 +6,24 @@ from secrets import trefle_token
 BASE_URL = "https://trefle.io/api/v1"
 
 def simple_search(token, search_term):
-    """Simple single-field search request"""
+    """Simple single-field search request. Returns JSON response as dict."""
     response = requests.get(f'{BASE_URL}/species/search', params={"q": search_term, "token": token})
     return response.json()["data"]
 
 def get_one_plant(token, plant_slug):
-    """Retrieves data for a specific plant."""
+    """Retrieves data for a specific plant. Returns JSON response as dict."""
     response = requests.get(f'{BASE_URL}/species/{plant_slug}', params={"token": token})
+    return response.json()["data"]
+
+
+red_flowers_range = {
+    "range[maximum_height_cm]": "20,120",
+    "filter[flower_color]": "red",
+    "filter[flower_conspicuous]": "true"
+}
+
+def advanced_search(token, search_terms):
+    """Multi-field search request to API. Returns JSON response as dict."""
+    search_terms["token"] = token
+    response = requests.get(f'{BASE_URL}/species', params=search_terms)
     return response.json()["data"]
