dfa4a1adb84a8f45b79d953f0d095b3d1a465ce7
Sara Strong <sara.n.strong@gmail.com>
2020-12-01 23:27:16 -0500

    Added todos

--- forms.py
+++ forms.py
@@ -13,17 +13,19 @@ class UserAddForm(FlaskForm):
 
 class UserEditForm(FlaskForm):
     """Form for editing user."""
-    password = PasswordField('Password', validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[Length(min=6)])
-    image_url = StringField('(Optional) Image URL')
-    header_image_url = StringField('(Optional) Header Image URL')
-    bio = StringField('Bio', validators=[Length(max=140)])
-    location = StringField('Location', validators=[Length(max=50)])
+    
 
 class LoginForm(FlaskForm):
     """Login form."""
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
+# TODO: Growing Area Form
+
+# TODO: Advanced Search Form
+
+
